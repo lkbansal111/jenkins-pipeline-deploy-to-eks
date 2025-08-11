2962b4212fed69c761553dec9b7ccdaf45dd8df5
Your Name <you@example.com>
2025-08-11 07:35:58 +0530

    changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "node_group_name_dev" {
-  value = module.eks.eks_managed_node_groups["dev"].node_group_name
-}
-
 output "region" {
   value = var.region
 }
